a48d49a27825c9881340db2647ab4b70724a0d2d
mactat <maciektatarski@gmail.com>
2021-11-19 13:39:41 +0000

    better error handeling

--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 NUM_OF_FILES=$(ls | grep -c input)
+FAILED=false
 for i in $(seq 1 $NUM_OF_FILES);
 do
+    TEST_FAILED=false
     start=`date +%s.%N`
-    python3 ../src/headless.py < "input$i.txt" > "output$i.txt"
+    python3 ../src/headless.py < "input$i.txt" 2>/dev/null > "output$i.txt" || TEST_FAILED=true && FAILED=true
     end=`date +%s.%N`
-
-    RESULT=$( cat "output$i.txt" )
     runtime=$( echo "$end - $start" | bc -l )
-    echo -e "Test $i \e[32mdone\e[0m.\nOutput: $RESULT.\nTime of the test: $runtime s\n"
-done
+    if [ "$TEST_FAILED" = true ]
+    then
+        echo -e "Test $i \e[031mfailed\e[0m.\nTime of the test: $runtime s\n"
+    else
+        RESULT=$( cat "output$i.txt" )
+        echo -e "Test $i \e[32mdone\e[0m.\nOutput:\n$RESULT.\nTime of the test: $runtime s\n"
+    fi
+done 
 
+if [ "$FAILED" = true ]
+then
+    exit 1
+fi
